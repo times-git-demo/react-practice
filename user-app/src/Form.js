7e527770a9b8393fb171ce5cc8c76b3a8f6543ee
Jai Arora <jai.arora@timesinternet.in>
2021-07-19 13:00:32 +0530

    UPDATE: form

--- user-app/src/Form.js
+++ user-app/src/Form.js
@@ -1,11 +1,15 @@
-import {useState} from "react";
+import { useState } from "react";
 
 const Form = () => {
-    const [input, setInput] = useState({firstname: "Jai"})
+    const [input, setInput] = useState({ firstname: "Jai" })
     return (
         <div>
             <h4>Form</h4>
-            <input value={input.firstname}></input>
+            <input value={input.firstname} onChange={(e) => { setInput({ ...input, firstname: e.target.value }) }}></input>
+            <input value={input.age} onChange={(e) => {
+                console.log(e);
+                setInput({ ...input, age: e.target.value })
+            }}></input>
 
         </div>
     )
