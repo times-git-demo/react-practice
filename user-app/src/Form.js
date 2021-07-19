e5350d79322bfe96cbf248656e14937f6d8c4b17
Jai Arora <jai.arora@timesinternet.in>
2021-07-19 16:09:05 +0530

    ADD: json-server UPDATE: form

--- user-app/src/Form.js
+++ user-app/src/Form.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
+import axios from "axios";
 
 const Form = () => {
-    const [input, setInput] = useState({ firstname: "Jai" })
+    const [input, setInput] = useState({firstname: "", age:"", joiningDate: ""})
     const handleInputChange = (e) => {
         console.log(e.target);
         setInput({ ...input, [e.target.name]: e.target.value })
+    }
+    const handleClick = () => {
+        const promise = axios.post('http://localhost:4200/users', input);
+        promise.then((response) => {
+            console.log(response);
+        })
+
     }
     return (
         <div>
             <h4>Form</h4>
             <input name="firstname" value={input.firstname} onChange={handleInputChange}></input>
             <input name="age" value={input.age} onChange={handleInputChange}></input>
+            <input name="joiningDate" type="date" value={input.joiningDate} onChange={handleInputChange}></input>
+            <button onClick={handleClick}>Save</button>
 
         </div>
     )
